14d50df3949bfc6b2af818fb78be638010b05507
izzymg <izzymhg@gmail.com>
2019-03-05 16:57:00 +1300

    Better comments

--- headerFix/editHtml.js
+++ headerFix/editHtml.js
@@ -1,3 +1,5 @@
+// Edits the HTML of the section
+
 (async() => {
 
   function toggleHtml() {
@@ -13,29 +15,32 @@
   let headerText;
   let tag;
 
+  // Await page interactivity
   chrome.runtime.sendMessage({ status: "Waiting for page load" });
   try {
     await mfOnDomContentLoaded();
-    // Wait some time for the editor to become interactive, no event can be caught
     await mfOnTime(2000);
   } catch(error) {
     return chrome.runtime.sendMessage({ error: "Waiting for page load failed, may have timed out" });
   }
 
   chrome.runtime.sendMessage({ status: "Checking HTML" });
+
   try {
+    // First child for text without an HTML tag, first ele for first html tag in section
     const firstEle = document.querySelector("#id_summary_editoreditable").firstElementChild;
     const firstChild = document.querySelector("#id_summary_editoreditable").firstChild;
 
-    // First element is an HTML element with a tag
+    // If the ele and child are the same, we'll use the ele
     if(firstEle.textContent === firstChild.textContent) {
-      // Grab text of element and tagname for searching later
       headerText = firstEle.textContent;
       tag = firstEle.tagName;
     } else {
+      // Use the raw text at the top
       headerText = firstChild.textContent;
     }
 
+    // Default if section has no content
     if(!firstEle || !firstEle.textContent) {
       // Use section name in value attribute of name change input as header text
       headerText = document.querySelector("#id_name_value").nodeValue; 
@@ -49,22 +54,24 @@
   }
 
   toggleHtml();
-
   chrome.runtime.sendMessage({ status: "Adding header1 class" });
 
   try {
     const textarea = document.querySelector("textarea#id_summary_editor");
+
+    // If we found an HTML element at the top, slice up to its closing tag off and replace with header1
     if(tag) {
-      // Grab index of the closing tag character (+ 3 for </>)
       const replaceTo = textarea.textContent.indexOf(`</${tag.toLowerCase()}>`) + tag.length + 3;
       textarea.textContent = html + textarea.textContent.slice(replaceTo);
     } else {
+      // Otherwise just insert header one and splice of the length of the text
       textarea.textContent = html + textarea.textContent.slice(headerText.length);
     }
   } catch(error) {
     console.error(error);
     return chrome.runtime.sendMessage({ error: "Failed to edit text area in editor" });
   }
+
 
   toggleHtml();
 
